cc357d296453b0db0515c0dff0417974bb71080a
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-05-27 14:49:05 -0500

    Agregando el acceso a la API de Turismoi

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const myHeaders = new Headers({
+  "Authorization": "Token token=f2b15a0105d45",
+  "Content-Type": "application/json"
+});
+
+const myInit = {
+  method: 'GET',
+  headers: myHeaders
+};
+
+fetch('https://turismoi.pe/api/v1/regions.json?only_with_packages=1', myInit)
+.then(rpta => rpta.json())
+.then(resultado => console.log(resultado))
+
+
 function App() {
   return (
     <div className="App">
