212ef0272bcdba495cb3061a6a30c88dc11c2047
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-05-28 16:42:58 -0500

    Implementando un select con opciones y pintando los tours en cards

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-const myHeaders = new Headers({
-  "Authorization": "Token token=f2b15a0105d45",
-  "Content-Type": "application/json"
-});
+class App extends Component {
+  
+  state = {
+    tours : [],
+    regions : []
+  }
 
-const myInit = {
-  method: 'GET',
-  headers: myHeaders
-};
+  myHeaders = new Headers({
+    "Authorization": "Token token=f2b15a0105d45",
+    "Content-Type": "application/json"
+  });
+  
+  myInit = {
+    method: 'GET',
+    headers: this.myHeaders
+  };
 
-fetch('https://turismoi.pe/api/v1/regions.json?only_with_packages=1', myInit)
-.then(rpta => rpta.json())
-.then(resultado => console.log(resultado))
+  getPackagesData = () => {
+    fetch('https://turismoi.pe/api/v1/packages.json?per_page=50', this.myInit)
+    .then(rpta => rpta.json())
+    .then(resultado => this.setState({tours: resultado.packages}))
+    // .then(res => console.log(res.packages))
+      // const arrTours = [];
+      // res.packages.forEach(element => {
+      //   arrTours.push(element);
+      // });
+      // console.log(arrTours);
+      // res.packages.map(elem => console.log(elem));
 
+      // const filtrar = (arr, nameRegion) => {
+      //   let arrFiltType = [];
+      //   arr.packages.filter(elem => elem.city_names.includes(nameRegion) ? arrFiltType.push(elem) : console.log(':('))
+      //   console.log(arrFiltType);
+      // };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      // filtrar(res, 'Cajamarca');
+  }
+  
+  getRegionsData = () => {
+    fetch('https://turismoi.pe/api/v1/regions.json?only_with_packages=1', this.myInit)
+    .then(rpta => rpta.json())
+    // .then(res => console.log(res.regions));
+    .then(resultado => this.setState({regions: resultado.regions}))
+  }
+
+  getIdRegion = (id) => {
+    console.log(id);
+  }
+
+  render() {
+    this.getRegionsData();
+    this.getPackagesData();
+    const regions = this.state.regions;
+    const tours = this.state.tours;
+    if(regions.length === 0) return null;
+    return (
+      <React.Fragment>
+        <div>
+          <select>
+            {regions.map(region => (
+              <option value={region.id} onChange={this.getIdRegion(region.id)}>{region.name}</option>
+            ))}
+          </select>
+        </div>
+        <div className="card">
+          {tours.map(tour => (
+            <img src={tour.principal_photo} className="card-img-top" alt="tour-foto-principal" />
+            <h5 className="card-title">{tour.name}</h5>
+            <p className="card-text">S/ {tour.price}</p>
+            <p className="card-text">{tour.days_and_nights}</p>
+          ))}
+        </div>
+      </React.Fragment>
+    )
+  }
 }
+
 
 export default App;
